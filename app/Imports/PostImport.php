95a2a37c40dac76972d426d6709c94a0a2aaa261
msdaungsangi <daungsangi@cgm-myanmar.com>
2023-10-17 08:39:55 +0630

    file upload download review fix

--- app/Imports/PostImport.php
+++ app/Imports/PostImport.php
@@ -4,40 +4,58 @@ namespace App\Imports;
 
 use App\Models\Post;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Maatwebsite\Excel\Concerns\ToCollection;
+use Maatwebsite\Excel\Concerns\WithValidation;
 
-class PostImport implements ToCollection
+class PostImport implements ToCollection, WithValidation
 {
     /**
      * @param Collection $row
      */
     public function collection(Collection $rows)
     {
-        $skipFirstRow = true;
+        DB::beginTransaction();
 
-        foreach ($rows as $row) {
-            if ($skipFirstRow) {
-                $skipFirstRow = false;
-                continue;
+        try {
+            $skipFirstRow = true;
+
+            foreach ($rows as $row) {
+                if ($skipFirstRow) {
+                    $skipFirstRow = false;
+                    continue;
+                }
+
+                $title = $row[0];
+                $description = $row[1];
+                $publicFlag = $row[2];
+
+                $publicFlagValidate = filter_var($publicFlag, FILTER_VALIDATE_BOOLEAN);
+
+                if ($publicFlagValidate || !$publicFlag) {
+                    Post::create([
+                        'title' => $title,
+                        'description' => $description,
+                        'public_flag' => $publicFlagValidate,
+                        'created_by' => Auth::user()->id,
+                    ]);
+                }
             }
 
-            $title = $row[0];
-            $description = $row[1];
-            $publicFlag = $row[2];
-            $createdBy = $row[3];
-            $updatedBy = $row[4];
-
-            $publicFlagVlidate = filter_var($publicFlag, FILTER_VALIDATE_BOOLEAN);
-
-            if (!empty($title) && ($publicFlagVlidate || !$publicFlagVlidate) && is_numeric($createdBy) && is_numeric($updatedBy)) {
-                Post::create([
-                    'title' => $title,
-                    'description' => $description,
-                    'public_flag' => $publicFlag,
-                    'created_by' => $createdBy,
-                    'updated_by' => $updatedBy,
-                ]);
-            }
+            DB::commit();
+        } catch (\Exception $e) {
+            DB::rollBack();
+            throw $e;
         }
+    }
+
+    public function rules(): array
+    {
+        return [
+            '0' => 'required|max:255',
+            '1' => 'nullable',
+            '2' => 'required|',
+        ];
     }
 }
